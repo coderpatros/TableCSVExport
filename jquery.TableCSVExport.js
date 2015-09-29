0520e45cc220fb538d8f4148a4951f0c87e80864
Patrick Dwyer <pat@patdwyer.com.au>
2015-09-29 10:57:30 +1000

    Add note about this being a fork of the "original"

--- jquery.TableCSVExport.js
+++ jquery.TableCSVExport.js
@@ -1,4 +1,4 @@
-    /*
+/*
 Based on the jQuery plugin found at http://www.kunalbabre.com/projects/TableCSVExport.php
 Re-worked by ZachWick for LectureTools Inc. Sept. 2011
 Copyright (c) 2011 LectureTools Inc.
@@ -8,6 +8,8 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This is a fork maintained by Patrick Dwyer (https://github.com/patros/TableCSVExport)
 */
 jQuery.fn.TableCSVExport = function (options) {
     var options = jQuery.extend({
